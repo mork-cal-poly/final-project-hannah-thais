1b3e2171a718ab6667c6b9da2a63d0300433f797
Hannah Yee <hyee02@calpoly.edu>
2024-03-18 01:45:32 -0700

    added clean pond, fish

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,6 @@
 let scene = 1;
 let buttonX, buttonY, buttonWidth, buttonHeight;
+let flowers;
 
 function setup() {
   // For ordering nodes in the DOM
@@ -49,6 +50,28 @@ function mouseClicked() {
       scene = 2; // Change scene to 2
     }
   }
+  else if (scene === 2) {
+    // If the button is clicked in Scene 1, transition to Scene 2
+    // if (
+    //   mouseX >= buttonX &&
+    //   mouseX <= buttonX + buttonWidth &&
+    //   mouseY >= buttonY &&
+    //   mouseY <= buttonY + buttonHeight
+    // ) {
+      scene = 3; // Change scene to 3
+    //}
+  }
+  else if (scene === 3) {
+    scene = 4;
+    flowers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    let i = 0
+    while (i<20){ 
+      flowers[i] = (random(width));
+      i++;
+      flowers [i] = random(height);
+      i++;
+    }
+  }
 }
 
   
@@ -95,5 +118,102 @@ let sLily;
 }
 
 
+function drawCleanLily(){
+  //---> *****clean pond***** <---
+  let xLily = [163, 22, 347, 55, 289, 97, 204, 381, 68, 134, 317, 291, 90, 225, 11];
+let yLily = [49, 306, 245, 88, 172, 384, 203, 57, 298, 119, 375, 30, 196, 300, 363];
+let rLily = [];
+let sLily = [];
 
+   rLily = [ 4.982, 1.235, 2.717, 5.634, 0.874, 3.890, 2.109, 6.006, 4.439, 4.982, 1.235, 2.717, 5.634, 0.874, 3.890];
+  sLily = [0.6, 1.2, 0.8, 1.0, 1.3, 0.7, 1.0, 1.1, 0.6, 1.2, 0.8, 1.3, 0.7, 1.1, 0.5];
+ 
+  for (let i = 0; i < 9; i++) {
+    rLily.push(0);
+    sLily.push(1); 
+  
+  }
+  background(180,245,255);
+
+  let i = 0;
+  while (i < 15) {
+
+    //---> *****Clean lilypads**** <---
+    push();
+    fill(100,190,150);
+    stroke(90,150,100);
+    translate(xLily[i], yLily[i]);
+    rotate(rLily[i]);
+    scale(sLily[i]);
+    arc(0, 0, 50, 50, -PI / 6, -PI / 2);
+    pop();
+    i++;
+
+    //rocks
+    stroke(105, 105, 102);
+    fill(143, 143, 138);
+    //rect(301,325, 100,80, 10,5,0,10)
+    quad(280, 340, 310, 295, 390, 280, 400, 360);
+    quad(160, 380, 130, 330, 170, 340, 190, 360);
+    quad(200, 140, 220, 85, 272, 90, 272, 140);
+  }
+
+  // Flower drawing
+  for (let count = 0; count < 6; count++) {
+    drawMainFlower(flowers[2*count], flowers[2*count+1], 20);
+  }
+}
+
+function drawMainFlower(x, y, size) {
+  push();
+  translate(x, y);
+
+  for (let r = 0; r < 2 * PI; r += 2 * PI / 10) {
+    push();
+    rotate(r);
+    fill(255);
+    noStroke();
+    ellipse(0, 10, 10, 30);
+    pop();
+  }
+
+  noStroke();
+  fill(255, 234, 141);
+  ellipse(0, 0, size);
+  pop();
+
+  drawSecondaryFlowers(x, y, size);
+}
+
+function drawSecondaryFlowers(x, y, size) {
+  for (let r = 0; r < 2 * PI; r += 2 * PI / 10) {
+    push();
+    translate(x + 100, y); 
+    rotate(r);
+    noStroke();
+    fill(255, 176, 247);
+    ellipse(0, 10, 10, 30);
+    pop();
+  }
+
+  noStroke();
+  fill(255, 234, 141);
+  ellipse(x + 100, y, size); 
+
+}
+ 
+
+function Fish(x, y){
+push();
+translate(x,y);
+noStroke();
+fill('orange');
+ellipse(0,0, 40, 20);
+triangle(-30,12, -30,-12, -12,0)
+fill('black');
+circle(10, -2, 5);
+
+pop();
+
+}
 
